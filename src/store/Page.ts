bbfcfda46644a94190eaa3fd0705ed30ea8f9cd6
Evert van Brussel <ecvanbrussel@gmail.com>
2021-02-11 19:50:30 +0100

    WIP

--- src/store/Page.ts
+++ src/store/Page.ts
@@ -1,43 +1,73 @@
 import Model, { SubCollection, CollectionQuery } from "rxfirestorm"
-import Delta from "quill-delta"
-
-export type Draft = Pick<Delta, "ops">
+import type { Props } from "rxfirestorm/dist/types"
+import { differenceWith, isEqual } from "lodash-es"
+import { Writable, writable, get } from "svelte/store"
 
 class ArchivedPage extends Model {
   static collection = "history"
 
-  public body = ""
-
-  constructor(init?: { body: string }) {
+  constructor(init?: any) {
     super(init)
     Object.assign(this, init)
   }
 }
 
-export default class Page extends Model {
+interface Page {
+  [key: string]: any
+}
+
+class Page extends Model {
   static collection = "pages"
 
-  public body = ""
-  public draft: Draft|null = null
+  public draft: any|null = null
   @SubCollection(ArchivedPage) public history!: CollectionQuery<typeof ArchivedPage>
-
-  get delta(): Delta|null {
-    return this.draft ? new Delta(this.draft) : null
-  }
-
-  set delta(newDelta: Delta|null) {
-    this.draft = newDelta ? { ...newDelta } : null
-  }
 
   constructor(init?: { body: string }) {
     super(init)
     Object.assign(this, init)
   }
 
-  async publish(body: string): Promise<void> {
-    await this.history.add({ body: this.body })
+  get hasUnfinishedDraft(): boolean {
+    if (!this.draft) return false
+    const draft = Object.entries(this.draft)
+    const published = Object.entries(this)
+    return differenceWith(draft, published, isEqual).length > 0
+  }
+
+  getDraftStore(): Writable<any> {
+    if (!this.draft) {
+      const data = this.getStrippedData() as any
+      delete data.history
+      delete data.draft
+      this.draft = data
+    }
+    if ("set" in this.draft) {
+      return this.draft
+    }
+    this.draft = writable(this.draft)
+    const ogSet = this.draft.set
+    this.draft.set = (newData: any) => {
+      ogSet(newData)
+      this.throttledSave()
+    }
+    return this.draft
+  }
+
+  async publishDraft(): Promise<void> {
+    const { draft, ...data } = this.getStrippedData() as any
+    delete data.history
+    await this.history.add(data)
+    Object.assign(this, draft)
     this.draft = null
-    this.body = body
     this.save()
   }
+
+  getStrippedData(): Exclude<Props<this>, "id"|"docRef"|"createdAt"|"updatedAt"|"throttledSave"> {
+    const data = super.getStrippedData();
+    (data as any).draft = get((data as any).draft)
+
+    return data
+  }
 }
+
+export default Page
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
   },
   theme: {
     extend: {},
+    boxShadow: {
+      up: "0 -1px 3px 0 rgb(0 0 0 / 10%), 0 -1px 2px 0 rgb(0 0 0 / 6%)",
+      "md-up": "0 -4px 6px -1px rgb(0 0 0 / 10%), 0 -2px 4px -1px rgb(0 0 0 / 6%)",
+    },
   },
   variants: {},
   plugins: [],
