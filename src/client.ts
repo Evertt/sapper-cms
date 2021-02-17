a0b109f859419f0e14f2762ed19ac3414b8e4bc1
Evert van Brussel <ecvanbrussel@gmail.com>
2021-02-17 18:42:15 +0100

    Another performance thingy

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-unresolved
 import * as sapper from "@sapper/app"
 
-sapper.start({ target: document.getElementById("app") as Node })
+sapper.start({ target: document.querySelector("body") as Node })
