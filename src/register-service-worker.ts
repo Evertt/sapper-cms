09d0c0afc8fbdaeaa4864e5e4edd49cacb232529
Evert van Brussel <ecvanbrussel@gmail.com>
2021-03-12 02:00:32 +0100

    Improve service worker a lot

--- src/register-service-worker.ts
+++ src/register-service-worker.ts
@@ -1,91 +1,49 @@
-/* eslint-disable no-underscore-dangle */
-function showRefreshUI(registration: any) {
-  // TODO: Display a toast or refresh UI.
+let refreshing: boolean
 
-  // This demo creates and injects a button.
+type SWInstalledEventListener = (registration: ServiceWorkerRegistration) => void
 
-  const button = document.createElement("button")
-  button.style.position = "absolute"
-  button.style.bottom = "24px"
-  button.style.left = "24px"
-  button.textContent = "This site has updated. Please click here to see changes."
-
-  button.addEventListener("click", () => {
-    if (!registration.waiting) {
-      // Just to ensure registration.waiting is available before
-      // calling postMessage()
-      return
-    }
-
-    button.disabled = true
-
+let swInstalledListener: SWInstalledEventListener = registration => {
+  // eslint-disable-next-line no-alert
+  if (registration.waiting && window.confirm("New version available! OK to refresh?")) {
     registration.waiting.postMessage("skipWaiting")
-  })
+  }
+}
 
-  document.body.appendChild(button)
+const respondToSWInstalled = (registration: ServiceWorkerRegistration) => () => {
+  swInstalledListener(registration)
+}
+
+export const onSWInstalled = (listener: SWInstalledEventListener): void => {
+  swInstalledListener = listener
 }
 
 function onNewServiceWorker(registration: ServiceWorkerRegistration, callback: () => void) {
-  if (registration.waiting) {
-    // SW is waiting to activate. Can occur if multiple clients open and
-    // one of the clients is refreshed.
-    return callback()
-  }
-
   function listenInstalledStateChange() {
     registration.installing?.addEventListener("statechange", event => {
       if ((event.target as any)?.state === "installed") {
-        // A new service worker is available, inform the user
         callback()
       }
     })
   }
 
-  if (registration.installing) {
-    return listenInstalledStateChange()
-  }
-
-  // We are currently controlled so a new SW may be found...
-  // Add a listener in case a new SW is found,
+  if (registration.waiting) return callback()
+  if (registration.installing) return listenInstalledStateChange()
   return registration.addEventListener("updatefound", listenInstalledStateChange)
 }
 
-if ("serviceWorker" in navigator /* && "SyncManager" in window */) {
+if (process.browser) {
   navigator.serviceWorker.register("/service-worker.js")
-    .then(registration => {
-      // Track updates to the Service Worker.
-      if (!navigator.serviceWorker.controller) {
-        // The window client isn"t currently controlled so it"s a new service
-        // worker that will activate immediately
-        console.log("No controller at the moment")
-        return
-      }
+    .then(registration => onNewServiceWorker(
+      registration, respondToSWInstalled(registration),
+    ))
 
-      // When the user asks to refresh the UI, we"ll need to reload the window
-      navigator.serviceWorker.addEventListener("controllerchange", function controllerchange() {
-        const t = this as any as { __allowControllerReload: boolean }
-        // Ensure refresh is only called once.
-        // This works around a bug in "force update on reload".
-        console.log(`this.__allowControllerReload: ${t.__allowControllerReload}`)
-        if (t.__allowControllerReload) {
-          console.log("New controller, but not refreshing.")
-          return
-        }
-
-        console.log("New controller found, reloading window")
-        t.__allowControllerReload = true
-        window.location.reload()
-      })
-
-      onNewServiceWorker(
-        registration,
-        () => showRefreshUI(registration),
-      )
-    })
+  navigator.serviceWorker.addEventListener("controllerchange", () => {
+    if (refreshing) return
+    refreshing = true
+    window.location.reload()
+  })
 
   navigator.serviceWorker.addEventListener("message", event => {
     if (event.data === "refresh") window.location.reload()
   })
 }
-
-export {}
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -14,15 +14,35 @@
   <main>
     <slot />
   </main>
+
+  {#if swReg?.waiting}
+    <div transition:slide class="bg-gray-900 text-gray-200 p-4 fixed bottom-0 right-0 m-4 rounded">
+      A new version is available.
+      <span class="text-yellow-500 underline cursor-pointer mx-1" on:click={skipWaiting}>Refresh</span>
+      <span class="text-yellow-500 underline cursor-pointer" on:click={_ => swReg = undefined}>Cancel</span>
+    </div>
+  {/if}
 </Modal>
 
 <script>
+  import { onSWInstalled } from "../register-service-worker"
   import FetchWrapper from "../components/FetchWrapper.svelte"
   import SessionObserver from "../components/SessionObserver.svelte"
   import UserObserver from "../components/UserObserver.svelte"
   import Nav from "../components/Nav.svelte"
+  import { slide } from "svelte/transition"
   import Modal from "svelte-simple-modal"
   export let segment = ""
+
+  let swReg: ServiceWorkerRegistration|undefined
+
+  const skipWaiting = () => {
+    const swRegClone = swReg
+    swReg = undefined
+    swRegClone?.waiting?.postMessage("skipWaiting")
+  }
+
+  onSWInstalled(reg => setTimeout(() => swReg = reg, 2000))
 </script>
 
 <style global>
